e3c5eb2d90ad100f4765dfe2847051c7c21a0d49
Viviane Dias <vivipirolad@gmail.com>
2022-01-13 17:08:08 -0300

    feat(serverless): adds lambda func and serveless config file

---
+++ handler.js
@@ -0,0 +1,15 @@
+const hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v2.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports = { hello };
